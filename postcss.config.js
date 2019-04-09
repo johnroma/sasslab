e262be5991389a1f80aafe2712c5da9ed2415d61
John Romani <john@anthood.com>
2019-04-09 17:45:26 +0200

    tests on general

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,5 @@
 module.exports = ctx => ({
-	map: ctx.options.map,
+	//map: ctx.options.map,
 	parser: ctx.file.extname === '.scss' ? 'css' : false,
 	// plugins: {
 	// 	'postcss-import': {
@@ -8,8 +8,25 @@ module.exports = ctx => ({
 	// 	cssnano: ctx.env === 'production' ? {} : false
 	// },
 	plugins: [
+		// require('postcss-import'),
 		require('autoprefixer')({
-			browsers: '> 5%'
-		})
+			//browsers: '> 5%'
+			browsers: [">0.5%",
+				"not ie 11",
+				"not op_mini all"
+			]
+		}),
+		require('cssnano')({
+			preset: ['default', {
+				normalizeWhitespace: false
+			}]
+		}),
+		// require("css-mqpacker")({
+		// 	sort: function (a, b) {
+		// 		return a.localeCompare(b);
+		// 	}
+		// })
+
+		//	require('postcss-nested')
 	]
 })
